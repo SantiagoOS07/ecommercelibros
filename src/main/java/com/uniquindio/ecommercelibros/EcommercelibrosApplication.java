ddc3f5711f319fb2706c78e3038e70b1bb4f6671
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2026-09-07 10:18:10 -0500

    Iniital commit

---
+++ src/main/java/com/uniquindio/ecommercelibros/EcommercelibrosApplication.java
@@ -0,0 +1,13 @@
+package com.uniquindio.ecommercelibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercelibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercelibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniquindio/ecommercelibros/EcommercelibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.ecommercelibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercelibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
