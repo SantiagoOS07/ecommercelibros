efc90fd9687f980925cf228269340fde2affe1d5
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2026-09-14 18:08:10 -0500

    refactor: add transitions on enums

--- src/main/java/com/uniquindio/ecommercelibros/domain/valueObject/EstadoUsuario.java
+++ src/main/java/com/uniquindio/ecommercelibros/domain/valueObject/EstadoUsuario.java
@@ -1,6 +1,22 @@
 package com.uniquindio.ecommercelibros.domain.valueObject;
 
 public enum EstadoUsuario {
+
     ACTIVO,
-    INACTIVO
+    INACTIVO;
+
+    public boolean puedeTransicionarA(EstadoUsuario siguiente) {
+        return switch (this) {
+            case ACTIVO -> siguiente == INACTIVO;
+            case INACTIVO -> siguiente == ACTIVO;
+        };
+    }
+
+    public boolean estaActivo() {
+        return this == ACTIVO;
+    }
+
+    public boolean estaInactivo() {
+        return this == INACTIVO;
+    }
 }
