84edb6b6f155cddd469140499c862f628b752c7d
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2026-09-14 18:11:57 -0500

    refactor: added transitions on enums

--- src/main/java/com/uniquindio/ecommercelibros/domain/valueObject/EstadoPedido.java
+++ src/main/java/com/uniquindio/ecommercelibros/domain/valueObject/EstadoPedido.java
@@ -1,10 +1,55 @@
 package com.uniquindio.ecommercelibros.domain.valueObject;
 
 public enum EstadoPedido {
-    PENDIENTE,
-    CONFIRMADO,
-    EN_PREPARACION,
+
+    EN_PROCESO,
+    ESPERANDO_PAGO,
     ENVIADO,
     ENTREGADO,
-    CANCELADO
+    CANCELADO;
+
+    public boolean puedeTransicionarA(EstadoPedido siguiente) {
+        return switch (this) {
+
+            case EN_PROCESO ->
+                    siguiente == ESPERANDO_PAGO ||
+                            siguiente == CANCELADO;
+
+            case ESPERANDO_PAGO ->
+                    siguiente == ENVIADO ||
+                            siguiente == CANCELADO;
+
+            case ENVIADO ->
+                    siguiente == ENTREGADO;
+
+            case ENTREGADO,
+                 CANCELADO ->
+                    false;
+        };
+    }
+
+    public boolean esFinal() {
+        return this == ENTREGADO ||
+                this == CANCELADO;
+    }
+
+    public boolean estaEnProceso() {
+        return this == EN_PROCESO;
+    }
+
+    public boolean estaEsperandoPago() {
+        return this == ESPERANDO_PAGO;
+    }
+
+    public boolean fueEnviado() {
+        return this == ENVIADO;
+    }
+
+    public boolean fueEntregado() {
+        return this == ENTREGADO;
+    }
+
+    public boolean fueCancelado() {
+        return this == CANCELADO;
+    }
 }
