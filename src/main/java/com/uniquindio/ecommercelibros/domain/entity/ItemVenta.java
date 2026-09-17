b1d70136a00e42d62af8221bae933d03c1523164
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2026-09-16 19:08:40 -0500

    refactor: connec entities with id's

--- src/main/java/com/uniquindio/ecommercelibros/domain/entity/ItemVenta.java
+++ src/main/java/com/uniquindio/ecommercelibros/domain/entity/ItemVenta.java
@@ -1,17 +1,35 @@
 package com.uniquindio.ecommercelibros.domain.entity;
 
+import com.uniquindio.ecommercelibros.domain.valueObject.ISBN;
 import com.uniquindio.ecommercelibros.domain.valueObject.Precio;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class ItemVenta {
-    private Libro libro;
+    private final UUID idItemVenta;
+    private ISBN isbnLibro;
     private int cantidad;
     private Precio precioUnitario;
     private Precio subTotal;
 
-    public ItemVenta(Libro libro, int cantidad, Precio precioUnitario, Precio subTotal) {
-        this.libro = libro;
+    public ItemVenta(UUID idItemVenta, ISBN isbnLibro, int cantidad, Precio precioUnitario, Precio subTotal) {
+        this.idItemVenta = UUID.randomUUID();
+        this.isbnLibro = isbnLibro;
         this.cantidad = cantidad;
         this.precioUnitario = precioUnitario;
         this.subTotal = subTotal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemVenta itemVenta = (ItemVenta) o;
+        return Objects.equals(idItemVenta, itemVenta.idItemVenta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(idItemVenta);
     }
 }
